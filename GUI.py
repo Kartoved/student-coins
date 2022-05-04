cad0ae6bef095d6c9a825db5612cf20f6008a4e3
KarToveD <67981770+Kartoved24@users.noreply.github.com>
2022-05-04 17:50:03 +0300

    Сделал сохранение в текстовый лог и отображение в окошке лога

--- GUI.py
+++ GUI.py
@@ -5,12 +5,12 @@ import json
 from datetime import datetime
 
 
-
 menu_def = [["Выбрать группу", ["начинающие", "средние", "сильные"]]]
 array_groups = ["начинающие", "средние", "сильные"]
 
 # текущая отформатированная дата и время
 now = datetime.now().strftime("%d-%m-%Y")
+
 
 def import_from_json(event):
     """импортирует фамилии из файла JSON
@@ -88,7 +88,8 @@ def simple_change_coins(student_name, quantity):
         quantity (_int_): количество баллов
     """
     dic_group[student_name] = dic_group[student_name] + quantity
-    group_window["STATUS"].update(value=f"{student_name} зачислено {quantity} коинов")
+    group_window["STATUS"].update(
+        value=f"{student_name} зачислено {quantity} коинов")
     export_to_json()
 
 
@@ -118,8 +119,8 @@ def make_group_window(event):
     folder = event
     for name, value in dic_group.items():
         group_layout.append([sg.Button(name), sg.Text(value),
-                             sg.Button("2", key=f"2 {name}"), 
-                             sg.Button("5", key=f"5 {name}"), 
+                             sg.Button("2", key=f"2 {name}"),
+                             sg.Button("5", key=f"5 {name}"),
                              sg.Button("-50", key=f"-50 {name}"), ])
         btn_list.append(f"2 {name}")
         btn_list.append(f"5 {name}")
@@ -143,21 +144,32 @@ def make_student_window(event):
                       [sg.Button(
                           "Добавить/вычесть"), sg.Button("Сохранить изменения"), sg.Button("Отмена")],
                       [sg.Multiline(default_text=student_log,
-                                    size=(400, 300), key="SAVE")],
+                                    size=(400, 300), key="STUDENT_LOG")],
                       ]
     return sg.Window(event, student_layout, finalize=True,  return_keyboard_events=True, size=(500, 800))
 
 
 def make_message(student_name, quantity_of_coins, action):
-    """создает сообщение о новой операции в логе
+    """создает сообщение о новой операции в текстовом логе и отображает изменения в окне STUDENT_LOG
+
+    Args:
+        student_name (_str_): имя ученика
+        quantity_of_coins (_type_): количество коинов
+        action (_type_): за что получил
     """
     with open(f"Лидеркоины\\{folder}\\{student_name}.txt", "a", encoding="utf-8") as file_txt:
-        file_txt.write(f"\n{now} {student_name} {quantity_of_coins} за {action}")
+        file_txt.write(
+            f"\n{now} {student_name} {quantity_of_coins} за {action}")
+    group_window["STATUS"].update(
+        f"\n{now} {student_name} {quantity_of_coins} за {action}")
+    import_from_text(student_name)
+    student_window["STUDENT_LOG"].update(student_log)
+
 
 def main():
     """основной цикл программы
     """
-    global group_window
+    global group_window, student_window
     main_window, group_window, student_window, change_coins_window = make_main_window(), None, None, None
     while True:  # Event Loop
         window, event, values = sg.read_all_windows()
@@ -176,7 +188,7 @@ def main():
                 coins_and_name = event.split()
                 simple_change_coins(coins_and_name[1], int(coins_and_name[0]))
         if event == "Сохранить изменения":
-            student_log = student_window["SAVE"].get()
+            student_log = student_window["STUDENT_LOG"].get()
             export_to_text(student_name, student_log)
             window.close()
         elif event == "Добавить/вычесть":
@@ -193,8 +205,6 @@ if __name__ == "__main__":
     main_window = make_main_window()
     main()
 
-#TODO сделай обновление лидеркоинов в ГУИ
-#TODO доделай окошко зачисления. Надо сделать поле "за что"
-#TODO доделай рефакторинг
-#TODO нужна ли менюшка??
-#TODO сохранение в лог файла
+# TODO доделай рефакторинг
+# TODO нужна ли менюшка??
+
